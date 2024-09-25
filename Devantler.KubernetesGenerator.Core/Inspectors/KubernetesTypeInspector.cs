c70f260d425aed71dd0ce77e1cebd1577b082da3
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-09-26 01:01:40 +0200

    Update KubernetesTypeInspector to be public

--- Devantler.KubernetesGenerator.Core/Inspectors/KubernetesTypeInspector.cs
+++ Devantler.KubernetesGenerator.Core/Inspectors/KubernetesTypeInspector.cs
@@ -8,19 +8,40 @@ namespace Devantler.KubernetesGenerator.Core.Inspectors;
 /// Thanks to @EdwardCooke for the initial implementation.
 /// </summary>
 /// <param name="inner"></param>
-sealed class KubernetesTypeInspector(ITypeInspector inner) : ITypeInspector
+public sealed class KubernetesTypeInspector(ITypeInspector inner) : ITypeInspector
 {
+  /// <summary>
+  /// Gets the inner type inspector.
+  /// </summary>
   public ITypeInspector Inner { get; } = inner;
 
+  /// <summary>
+  /// Gets the name of the enum value.
+  /// </summary>
+  /// <param name="enumType"></param>
+  /// <param name="name"></param>
+  /// <returns></returns>
   public string GetEnumName(Type enumType, string name) => Inner.GetEnumName(enumType, name);
 
+  /// <summary>
+  /// Gets the value of the enum.
+  /// </summary>
+  /// <param name="enumValue"></param>
+  /// <returns></returns>
   public string GetEnumValue(object enumValue) => Inner.GetEnumValue(enumValue);
 
-  public IEnumerable<IPropertyDescriptor> GetProperties(Type systemType, object? container)
+  /// <summary>
+  /// Gets the properties of the system type.
+  /// </summary>
+  /// <param name="type"></param>
+  /// <param name="container"></param>
+  /// <returns></returns>
+  public IEnumerable<IPropertyDescriptor> GetProperties(Type type, object? container)
   {
-    var properties = Inner.GetProperties(systemType, container).ToList();
+    ArgumentNullException.ThrowIfNull(type, nameof(type));
+    var properties = Inner.GetProperties(type, container).ToList();
     var skipped = new List<string>();
-    if (systemType.FullName?.StartsWith("k8s.Models", StringComparison.OrdinalIgnoreCase) ?? true)
+    if (type.FullName?.StartsWith("k8s.Models", StringComparison.OrdinalIgnoreCase) ?? true)
     {
       foreach (string? propName in new[] { "apiVersion", "kind", "metadata", "type" })
       {
@@ -47,5 +68,14 @@ sealed class KubernetesTypeInspector(ITypeInspector inner) : ITypeInspector
     }
   }
 
+  /// <summary>
+  /// Gets the property of the type.
+  /// </summary>
+  /// <param name="type"></param>
+  /// <param name="container"></param>
+  /// <param name="name"></param>
+  /// <param name="ignoreUnmatched"></param>
+  /// <param name="caseInsensitivePropertyMatching"></param>
+  /// <returns></returns>
   public IPropertyDescriptor GetProperty(Type type, object? container, string name, [MaybeNullWhen(true)] bool ignoreUnmatched, bool caseInsensitivePropertyMatching) => Inner.GetProperty(type, container, name, ignoreUnmatched, caseInsensitivePropertyMatching);
 }
