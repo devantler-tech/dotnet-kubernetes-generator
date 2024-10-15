bb5b91d13832e057fbaee5bf67f01626e0ca812c
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2024-10-15 21:31:22 +0200

    Added KSailContainerEngine tot the KSailCluster config (#52)

---
+++ Devantler.KubernetesGenerator.KSail/Models/KSailContainerEngine.cs
@@ -0,0 +1,12 @@
+namespace Devantler.KubernetesGenerator.KSail.Models;
+
+/// <summary>
+/// The container engine to use for the KSail cluster.
+/// </summary>
+public enum KSailContainerEngine
+{
+  /// <summary>
+  /// Docker container engine.
+  /// </summary>
+  Docker
+}
