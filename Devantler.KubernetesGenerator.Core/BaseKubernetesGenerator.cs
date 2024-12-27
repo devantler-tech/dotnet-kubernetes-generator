babce7664545eea36e1ea895e75f00254871293e
Nikolai Emil Damm <neq@energinet.dk>
2024-12-27 14:52:10 +0100

    Refactor BaseKubernetesGenerator to support omitting default values in serialization
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

--- Devantler.KubernetesGenerator.Core/BaseKubernetesGenerator.cs
+++ Devantler.KubernetesGenerator.Core/BaseKubernetesGenerator.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel;
 using Devantler.KubernetesGenerator.Core.Converters;
 using Devantler.KubernetesGenerator.Core.Inspectors;
+using YamlDotNet.Core;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization.ObjectFactories;
+using YamlDotNet.Serialization.ObjectGraphVisitors;
 using YamlDotNet.System.Text.Json;
 
 
@@ -13,13 +17,27 @@ namespace Devantler.KubernetesGenerator.Core;
 /// <typeparam name="T"></typeparam>
 public class BaseKubernetesGenerator<T> : IKubernetesGenerator<T> where T : class
 {
-  readonly ISerializer _serializer = new SerializerBuilder()
-    .DisableAliases()
-    .WithTypeInspector(inner => new KubernetesTypeInspector(new SystemTextJsonTypeInspector(inner)))
-    .WithTypeConverter(new IntstrIntOrStringTypeConverter())
-    .WithTypeConverter(new ResourceQuantityTypeConverter())
-    .ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull)
-    .WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+  readonly ISerializer _serializer;
+
+  /// <summary>
+  /// Initializes a new instance of the <see cref="BaseKubernetesGenerator{T}"/> class.
+  /// </summary>
+  /// <param name="omitDefaults"></param>
+  public BaseKubernetesGenerator(bool omitDefaults = false)
+  {
+    var serializerBuilder = new SerializerBuilder()
+      .DisableAliases()
+      .WithTypeInspector(inner => new KubernetesTypeInspector(new SystemTextJsonTypeInspector(inner)))
+      .WithTypeConverter(new IntstrIntOrStringTypeConverter())
+      .WithTypeConverter(new ResourceQuantityTypeConverter())
+      .WithNamingConvention(CamelCaseNamingConvention.Instance);
+
+    serializerBuilder = omitDefaults
+      ? serializerBuilder.WithEmissionPhaseObjectGraphVisitor(args => new OmitNullAndDefaultsObjectGraphVisitor(args.InnerVisitor))
+      : serializerBuilder.ConfigureDefaultValuesHandling(DefaultValuesHandling.OmitNull);
+    _serializer = serializerBuilder.Build();
+  }
+
 
   /// <summary>
   /// Generates Kubernetes resources from the given model and writes them to the output path.
@@ -35,6 +53,7 @@ public class BaseKubernetesGenerator<T> : IKubernetesGenerator<T> where T : clas
     {
       _ = Directory.CreateDirectory(outputDirectory);
     }
+
     string yaml = _serializer.Serialize(model);
 
     if (!yaml.StartsWith("---", StringComparison.Ordinal))
@@ -49,5 +68,16 @@ public class BaseKubernetesGenerator<T> : IKubernetesGenerator<T> where T : clas
     {
       await File.AppendAllTextAsync(outputPath, yaml, cancellationToken).ConfigureAwait(false);
     }
+  }
+}
+
+class OmitNullAndDefaultsObjectGraphVisitor(IObjectGraphVisitor<IEmitter> nextVisitor) : ChainedObjectGraphVisitor(nextVisitor)
+{
+  static object? GetDefault(Type type) => new DefaultObjectFactory().CreatePrimitive(type);
+
+  public override bool EnterMapping(IPropertyDescriptor key, IObjectDescriptor value, IEmitter context, ObjectSerializer serializer)
+  {
+    object? defaultValue = key.GetCustomAttribute<DefaultValueAttribute>()?.Value ?? GetDefault(key.Type);
+    return value.Value is not null && !Equals(value.Value, defaultValue) && base.EnterMapping(key, value, context, serializer);
   }
 }
