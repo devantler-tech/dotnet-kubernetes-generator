a5a12ec05933ea7393fccbf8346c467948afbf51
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-02-01 02:15:23 +0100

    Enhance FluxKustomization model with API version and kind properties; refactor generator and add post-build substitution support (#209)
    Signed-off-by: Nikolai Emil Damm <neq@energinet.dk>

---
+++ src/Devantler.KubernetesGenerator.Flux/Models/Kustomization/FluxKustomizationSpecPostBuildSubstituteFrom.cs
@@ -0,0 +1,22 @@
+namespace Devantler.KubernetesGenerator.Flux.Models.Kustomization;
+
+/// <summary>
+/// A source to substitute from in the Flux Kustomization object.
+/// </summary>
+public class FluxKustomizationSpecPostBuildSubstituteFrom
+{
+  /// <summary>
+  /// The kind of the source to substitute from.
+  /// </summary>
+  public required FluxConfigRefKind Kind { get; set; }
+
+  /// <summary>
+  /// The name of the source to substitute from.
+  /// </summary>
+  public required string Name { get; set; }
+
+  /// <summary>
+  /// Whether the source is optional.
+  /// </summary>
+  public bool Optional { get; set; }
+}
